ec4918d5b61339cbaa1e244110bb7354d575dba8
Sang <rsw7876@naver.com>
2025-03-10 21:37:41 +0900

    nodejs_cookie_read

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -1,10 +1,21 @@
 var http = require('http');
+var cookie = require('cookie');
 http.createServer(function(request, response){
-    // response.writeHead(200, {
-    //     'set-cookie': [
-    //         'yummy_cookie=choco',
-    //         'tasty_cookie=strawberry',
-    //     ]
-    // });
+    // cookie read
+    console.log(request.headers.cookie)
+
+    var coookies = {};
+    if (request.headers.cookie !== undefined) {
+        // 객체로 반환
+        var coookies = cookie.parse(request.headers.cookie);
+    }    
+    console.log(coookies.yummy_cookie);
+    
+    response.writeHead(200, {
+        'set-cookie': [
+            'yummy_cookie=choco',
+            'tasty_cookie=strawberry',
+        ]
+    });
     response.end('Cookie!!');
 }).listen(3000);
